b4b8cff7ae9827d174a0c02798bf3b728c2b2dcf
Khemmie-Ray <atokemmy@gmail.com>
2023-12-13 01:28:58 +0100

    corrections to failed deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Header from './(components)/header'
 import Content from './(components)/content'
-import Footer from './(components)/Footer'
+import Footer from './(components)/footer'
 
 const Home = () => {
   return (
